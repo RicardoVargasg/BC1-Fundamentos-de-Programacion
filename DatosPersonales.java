33315256c69da6959a255eb4ae99e9601b0c47d3
RicardoVargasg <ricardo.vargas@educa.jcyl.es>
2025-10-14 10:58:39 +0200

    first commit

---
+++ DatosPersonales.java
@@ -0,0 +1,8 @@
+public class DatosPersonales {
+	public static void main(String[] args) {
+		String nombre = "Ricardo Vargas";
+		int edad = 19;
+		System.out.println("Nombre: " + nombre);
+		System.out.println("Edad: " + edad + " años");
+	}
+}
