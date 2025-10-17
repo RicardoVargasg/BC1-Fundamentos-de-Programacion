9a9b6123dae4244c9ec50611fdda825d62ad5061
RicardoVargasg <ricardo.vargas@educa.jcyl.es>
2025-10-17 08:45:05 +0200

    first commit

---
+++ NombreEdad.java
@@ -0,0 +1,16 @@
+//Programa que solicita nombre y edad con mensaje personalizado
+
+import java.util.Scanner;
+public class NombreEdad {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce tu nombre; ");
+        String nombre = scanner.nextLine();
+
+        System.out.print("Introduce tu edad: ");
+        int edad= scanner.nextInt();
+
+        System.out.println("Hola, " + nombre.toUpperCase() + " . Tienes " + edad + "años.");
+    }
+}
