fcca7f39ebe44daeba787b380fca55d03dc1928e
RicardoVargasg <ricardo.vargas@educa.jcyl.es>
2025-10-17 09:03:43 +0200

    first commit

---
+++ VariablesTiposDatos.java
@@ -0,0 +1,17 @@
+//Programa que declara valores de distintos datos
+
+import java.util.Scanner;
+
+public class VariablesTiposDatos {
+    public static void main(String[] args){
+        int edad = 22;
+        double sueldo = 1900;
+        boolean Empleado = true;
+        String nombre = "Marcelo";
+
+        System.out.println("Nombre: " + nombre);
+		System.out.println("Edad: " + edad);
+		System.out.println("Salario: " + sueldo);
+		System.out.println("¿Trabaja aquí?: " + Empleado);
+    }
+}
